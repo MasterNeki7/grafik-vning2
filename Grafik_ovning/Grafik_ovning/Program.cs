01bc25df829da33ce711592632cc28bf1ca241e4
Neo Babic <neo.babic@elev.ga.ntig.se>
2022-10-21 09:08:38 +0200

    idk

--- Grafik_ovning/Grafik_ovning/Program.cs
+++ Grafik_ovning/Grafik_ovning/Program.cs
@@ -1,19 +1,25 @@
 ﻿using Raylib_cs;
 
-Raylib.InitWindow(800, 600, "grafiktest");
+Raylib.InitWindow(1920, 1080, "grafiktest");
+{
+    
+}
 Raylib.SetTargetFPS(60);
+
 
 Color hotPink = new Color(255, 105, 180, 255);
 Color whitepink = new Color(245, 160, 233, 255);
 
 
 Texture2D player = Raylib.LoadTexture("F14_1.png");
-Texture2D bkg = Raylib.LoadTexture("vietnam.png");
+Texture2D bkg = Raylib.LoadTexture("mil_base.png");
+Texture2D bullet = Raylib.LoadTexture("bullet_1.png");
 
 Rectangle playerRect = new Rectangle(400, 300, 64, 64);
+Rectangle bulletRect = new Rectangle((int)playerRect.x, (int)playerRect.y, 32, 32);
 
 int speed = 5;
-
+//69
 while (Raylib.WindowShouldClose() == false)
 {
     if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
@@ -32,14 +38,18 @@ while (Raylib.WindowShouldClose() == false)
     {
         playerRect.y -= speed;
     }
-
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+    {
+        Raylib.DrawTexture(bullet, (int)bulletRect.x, (int)bulletRect.y, Color.BLANK);
+        bulletRect.y += 1;
+    }
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(hotPink);
 
     Raylib.DrawTexture(bkg, 0, 0, Color.WHITE);
     Raylib.DrawTexture(player, (int)playerRect.x, (int)playerRect.y, Color.GRAY);
-
+    
 
 
     Raylib.EndDrawing();
