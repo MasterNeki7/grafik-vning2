4f9f500484cb5457591b6bc3e3f8aefc54965ced
Neo Babic <neo.babic@elev.ga.ntig.se>
2022-12-01 14:58:56 +0100

    yens

--- Grafik_ovning/Grafik_ovning/Program.cs
+++ Grafik_ovning/Grafik_ovning/Program.cs
@@ -8,18 +8,22 @@ int bkgSpeed = 3;
 int bkg1PosY = 0;
 int bkg2PosY = -1080;
 int speed = 10;
+int bulletSpeed = 5;
+
+
 
 Texture2D player = Raylib.LoadTexture("F14_1.png");
 Texture2D bkg1 = Raylib.LoadTexture("galax 2.png");
 Texture2D bkg2 = Raylib.LoadTexture("galax 1.png");
 Texture2D bullet = Raylib.LoadTexture("bullet_1.png");
 
-Rectangle playerRect = new Rectangle(400, 300, 64, 64);
-Rectangle bulletRect = new Rectangle((int)playerRect.x, (int)playerRect.y, 32, 32);
+Rectangle playerRect = new Rectangle(960, 540, 64, 64);
+Rectangle bulletRect = new Rectangle((int)playerRect.x, (int)playerRect.y, bullet.width, bullet.height);
 Rectangle bkg1rect = new Rectangle(0, 0, (int)bkg1.width, (int)bkg1.height);
 Rectangle bkg2rect = new Rectangle(0, -1080, (int)bkg2.width, (int)bkg2.height);
 
-
+float bulletPosX = playerRect.x - bullet.width;
+float bulletPosY = playerRect.y;
 
 //69
 while (Raylib.WindowShouldClose() == false)
@@ -40,30 +44,40 @@ while (Raylib.WindowShouldClose() == false)
     {
         playerRect.y -= speed;
     }
-    if(bkg1PosY > 1080)
+
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+    {
+        bulletPosX = playerRect.x + bullet.width;
+        bulletPosY = playerRect.y;
+    }
+
+    if (bkg1PosY > 1080)
     {
         bkg1PosY = -1080;
     }
-    if(bkg2PosY > 1080)
+    if (bkg2PosY > 1080)
     {
         bkg2PosY = -1080;
     }
-    if(playerRect.x > 1920 - player.width)
+    if (playerRect.x > 1920 - player.width)
     {
         playerRect.x = 1920 - player.width;
     }
-    if(playerRect.x < 0)
+    if (playerRect.x < 0)
     {
         playerRect.x = 0;
     }
-    if(playerRect.y > 1080 - player.height)
+    if (playerRect.y > 1080 - player.height)
     {
         playerRect.y = 1080 - player.height;
     }
-    if(playerRect.y < 0)
+    if (playerRect.y < 0)
     {
         playerRect.y = 0;
     }
+
+
+    bulletPosY -= bulletSpeed;
 
     bkg1PosY += bkgSpeed;
 
@@ -77,6 +91,7 @@ while (Raylib.WindowShouldClose() == false)
 
     Raylib.DrawTexture(player, (int)playerRect.x, (int)playerRect.y, Color.GRAY);
 
+    Raylib.DrawTexture(bullet, (int)bulletPosX, (int)bulletPosY, Color.RED);
 
 
     Raylib.EndDrawing();
